a70f541b8766385a87d833efc7277cc0e071bf05
jersonv98 <jjvasquezp@utn.edu.ec>
2020-03-02 22:59:40 -0500

    feat: modified view

--- registroEstudiantes/Controllers/MateriasController.cs
+++ registroEstudiantes/Controllers/MateriasController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using registroEstudiantes.Models;
 
 namespace registroEstudiantes.Controllers
 {
+    [Authorize]
     public class MateriasController : Controller
     {
         private readonly registroEstudiantesContext _context;
